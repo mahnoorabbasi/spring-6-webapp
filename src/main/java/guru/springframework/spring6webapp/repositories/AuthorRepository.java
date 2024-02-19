c77caf7a8d45de15342d69ff4790729b35cc4ee0
mahnoor <mabbasi@xavor.com>
2024-02-19 15:17:19 +0500

    added repos

---
+++ src/main/java/guru/springframework/spring6webapp/repositories/AuthorRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.spring6webapp.repositories;
+
+import guru.springframework.spring6webapp.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.Repository;
+
+public interface AuthorRepository extends CrudRepository<Author,Long> {
+}
